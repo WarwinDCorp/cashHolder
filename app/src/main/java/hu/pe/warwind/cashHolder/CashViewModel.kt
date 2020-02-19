d38ca26db61599f878ec7f39f153b151bf8ba31f
Наливайко Юрій <nalyvaiko.yu@novaposhta.ua>
2020-02-19 17:03:00 +0200

    try to calculate Outcome

--- app/src/main/java/hu/pe/warwind/cashHolder/CashViewModel.kt
+++ app/src/main/java/hu/pe/warwind/cashHolder/CashViewModel.kt
@@ -3,6 +3,7 @@ package hu.pe.warwind.cashHolder
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.viewModelScope
 import hu.pe.warwind.cashHolder.Room.CashRepository
 import hu.pe.warwind.cashHolder.Room.CashTable
@@ -15,6 +16,7 @@ class CashViewModel(application: Application) : AndroidViewModel(application) {
     val allCash: LiveData<List<CashTable>>
     val outCome: LiveData<Double>
     val inCome: LiveData<Double>
+    val differenceLiveData = MediatorLiveData<Double>()
 
     init {
         val cashTableDao = CashTableRoomDatabase.getDatabase(application, viewModelScope).cashTableDao()
@@ -22,6 +24,19 @@ class CashViewModel(application: Application) : AndroidViewModel(application) {
         allCash = repository.allCash
         inCome = repository.inCome
         outCome = repository.outCome
+        differenceLiveData.value = 0.0
+
+        differenceLiveData.addSource(inCome, {
+            it?.let{
+                differenceLiveData.value - differenceLiveData.value?.plus(it)
+            }
+        })
+
+        differenceLiveData.addSource(outCome, {
+            it?.let{
+                differenceLiveData.value - differenceLiveData.value?.minus(it)
+            }
+        })
     }
 
     fun insert(cashTable: CashTable) = viewModelScope.launch {
